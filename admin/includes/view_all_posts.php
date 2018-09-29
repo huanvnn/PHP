afac04b178e07f640895f9cc9cf05e6be238ff58
huanle <huanlexuan23818@gmail.com>
2018-09-29 17:16:21 +0700

    display cate_title in view_all_post

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -19,26 +19,44 @@
         $all_pots_query = mysqli_query($connection, $query);
 
         while ($rows = mysqli_fetch_assoc($all_pots_query)) {
-        ?>       
+            $post_id = $rows['post_id'];
+            $post_category_id =  $rows['post_category_id'];
+            $post_title =$rows['post_title'];
+            $post_author = $rows['post_author'];
+            $post_date= $rows['post_date'];
+            $post_image= $rows['post_image'];
+            $post_tag=$rows['post_tag'];
+            $post_content = $rows['post_content'];
+            $post_comment_count=$rows['post)comment_count'];
+            $post_status = $rows['post_status'];
 
-            <tr>
-                <td><?php echo $rows['post_id']; ?></td>
-                <td><?php echo $rows['post_category_id']; ?></td>
-                <td><?php echo $rows['post_title']; ?></td>
-                <td><?php echo $rows['post_author']; ?></td>
-                <td><?php echo $rows['post_date']; ?></td>
-                <td><img class="img-responsive" src="../images/<?php echo $rows['post_image']; ?>" ></td>
-                <td><?php echo $rows['post_tag']; ?></td>
-                <td><?php echo $rows['post_content']; ?></td>
-                <td><?php echo $rows['post_comment_count']; ?></td>
-                <td><?php echo $rows['post_status']; ?></td>
-                <td><a href="posts.php?source=edit_post&p_id=<?php echo $rows['post_id'];?>">Edit</a></td>
-                <td><a href="posts.php?delete=<?php echo $rows['post_id'];?>">Delete</a></td>
-                
+            echo   "<tr>";
+            echo   "<td> $post_id </td>"; 
+            
+            $query= "SELECT * FROM categories WHERE cate_id=$post_category_id";
+            $query_cate = mysqli_query($connection, $query);
+            while ($rows= mysqli_fetch_assoc($query_cate)) {
+                $cate_id = $rows['cate_id'];
+                $cate_title = $rows['cate_title'];
               
-            </tr>
+                echo   "<td> $cate_title </td>";  
+            }
 
-    <?php  }?>
+            
+
+            echo   "<td> $post_title </td>";
+            echo   "<td> $post_author</td>";
+            echo   "<td> $post_date</td>";
+            echo   "<td><img class='img-responsive' src='../images/$post_image' ></td>";
+            echo   "<td>$post_tag</td>";
+            echo   "<td>$post_content</td>";
+            echo   "<td>$post_comment_count</td>";
+            echo   "<td>$post_status</td>";
+            echo    "<td><a href='posts.php?source=edit_post&p_id=$post_id'>Edit</a></td>";
+            echo    "<td><a href='posts.php?delete=$post_id'>Delete</a></td>";             
+            echo "</tr>";
+      }
+      ?>
     <?php
        deletePostByID();
     ?>
